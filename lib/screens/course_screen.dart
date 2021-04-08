9e5b365b13420fc896f059b2cc6f9034da214e07
kmranrg <kmranrg@gmail.com>
2021-04-08 17:59:30 +0530

    creating a vertical scrolling list

--- lib/screens/course_screen.dart
+++ lib/screens/course_screen.dart
@@ -1,10 +1,8 @@
 import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gurucool/model/course.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
-
 import '../constants.dart';
 
 class CourseScreen extends StatefulWidget {
@@ -17,7 +15,6 @@ class CourseScreen extends StatefulWidget {
 }
 
 class _CourseScreenState extends State<CourseScreen> {
-
   Widget indicators() {
     List<Widget> indicators = [];
 
@@ -68,7 +65,7 @@ class _CourseScreenState extends State<CourseScreen> {
           ],
           minHeight: 0.0,
           maxHeight: MediaQuery.of(context).size.height * 0.95,
-          panel: Container(),
+          panel: CourseSectionsScreen(),
           body: SingleChildScrollView(
             child: Column(
               children: [
@@ -150,8 +147,8 @@ class _CourseScreenState extends State<CourseScreen> {
                                         decoration: BoxDecoration(
                                           borderRadius:
                                               BorderRadius.circular(12.0),
-                                          color:
-                                              kPrimaryLabelColor.withOpacity(0.8),
+                                          color: kPrimaryLabelColor
+                                              .withOpacity(0.8),
                                         ),
                                         child: Icon(
                                           Icons.close,
@@ -317,7 +314,8 @@ class _CourseScreenState extends State<CourseScreen> {
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 28.0, vertical: 24.0),
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 28.0, vertical: 24.0),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -378,6 +376,176 @@ class _CourseScreenState extends State<CourseScreen> {
                       ),
                     ],
                   ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CourseSectionsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: kBackgroundColor,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(34.0),
+        ),
+      ),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(32.0),
+            decoration: BoxDecoration(
+              color: kCardPopupBackgroundColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(34.0),
+                bottomLeft: Radius.circular(34.0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: kShadowColor,
+                  offset: Offset(0, 12),
+                  blurRadius: 32.0,
+                ),
+              ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "Course Sections",
+                  style: kTitle2Style,
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  "12 sections",
+                  style: kSubtitleStyle,
+                ),
+              ],
+            ),
+          ),
+          CourseSectionList(),
+          SizedBox(
+            height: 32.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CourseSectionList extends StatelessWidget {
+
+  List<Widget> courseSectionsWidgets() {
+    List<Widget> cards = [];
+    for(var course in courseSections){
+      cards.add(
+        Padding(
+          padding: EdgeInsets.only(bottom: 20.0,),
+          child: CourseSectionCard(course: course,),
+        ),
+      );
+    }
+
+    cards.add(
+      Padding(
+        padding: EdgeInsets.only(top: 12.0),
+        child: Text(
+          "No more Sections to view, look\nfor more in our courses",
+          style: kCaptionLabelStyle,
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+
+    return cards;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView(
+        children: courseSectionsWidgets(),
+      ),
+    );
+  }
+}
+
+
+class CourseSectionCard extends StatelessWidget {
+  CourseSectionCard({@required this.course});
+
+  final Course course;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Container(
+        height: 120.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(41.0),
+          gradient: course.background,
+          boxShadow: [
+            BoxShadow(
+              color: course.background.colors[0].withOpacity(0.3),
+              blurRadius: 30.0,
+              offset: Offset(0, 20),
+            ),
+            BoxShadow(
+              color: course.background.colors[1].withOpacity(0.3),
+              blurRadius: 30.0,
+              offset: Offset(0, 20),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(41.0),
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 32.0,
+            ),
+            child: Stack(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Image.asset(
+                      'asset/illustrations/${course.illustration}',
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            course.courseSubtitle,
+                            style: kCardSubtitleStyle,
+                          ),
+                          SizedBox(
+                            height: 6.0,
+                          ),
+                          Text(
+                            course.courseTitle,
+                            style: kCardTitleStyle,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Spacer(),
+                  ],
                 ),
               ],
             ),
